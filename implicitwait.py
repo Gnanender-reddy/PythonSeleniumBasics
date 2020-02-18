cb10377c87d8b6dd9212eb46e9cf10bf55acb585
Gnanender-reddy <gnanenderreddy3399@gmail.com>
2020-02-18 18:29:12 +0530

    Adding Implicit wait Program

--- implicitwait.py
+++ implicitwait.py
@@ -1,16 +1,9 @@
-"""
-@Author : P.Gnanender Reddy
-@Since : Feb'2020
-@keywords: selenium.
-@Description:This code is for python selenium basics which has conditional commands.
-"""
-
-
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 driver=webdriver.Chrome()
 driver.get("https://www.irctc.co.in/nget/profile/user-registration")
+driver.implicitly_wait(10)
 element=driver.find_element_by_name("userName")
 print(element.is_displayed())
 print(element.is_enabled())
@@ -21,7 +14,3 @@ print(element1.is_enabled())
 Male_radio=driver.find_element_by_css_selector("input[value=M]")
 print(Male_radio.is_selected(),"------------>")#it checks the status of radio button
 driver.close()
-# element.send_keys("0000")
-# element1.send_keys("00000")
-# driver.find_element_by_id('u_0_4').click()
-# time.sleep(3)
--- navigationcommands.py
+++ navigationcommands.py
@@ -1,5 +1,10 @@
+"""
+@Author : P.Gnanender Reddy
+@Since : Feb'2020
+@keywords: selenium.
+@Description:This code is for python selenium basics which has navigation commands.
+"""
 import time
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 driver=webdriver.Chrome()
