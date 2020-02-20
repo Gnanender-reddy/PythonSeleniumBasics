9b71225b11ad07a695cd1691612121ad67a07904
Gnanender-reddy <gnanenderreddy3399@gmail.com>
2020-02-20 15:39:24 +0530

    Adding windows file

---
+++ handlingwindows.py
@@ -0,0 +1,21 @@
+"""
+@Author : P.Gnanender Reddy
+@Since : Feb'2020
+@keywords: selenium.
+@Description:This code is for python selenium basics which has handling browsers windows concept.
+"""
+
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver=webdriver.Chrome()
+
+driver.maximize_window()
+driver.get("http://demo.automationtesting.in/Windows.html")
+driver.find_element_by_xpath("//*[@id='Tabbed']/a/button").click()
+handles=driver.window_handles
+for handle in handles:
+    driver.switch_to.window(handle)
+    print(driver.title)
+driver.quit()
