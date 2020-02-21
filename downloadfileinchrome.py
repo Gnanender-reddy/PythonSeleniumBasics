9d5b0577ee1d1d24becb74fc4694e35415438108
Gnanender-reddy <gnanenderreddy3399@gmail.com>
2020-02-21 17:58:32 +0530

    adding one more file

---
+++ downloadfileinchrome.py
@@ -0,0 +1,23 @@
+"""
+@Author : P.Gnanender Reddy
+@Since : Feb'2020
+@keywords: selenium.
+@Description:This code is for python selenium basics which has downloading files  in chrome concept.
+"""
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+#to change the chrome settings for downloading files
+chromeoptions=Options()
+chromeoptions.add_experimental_option("prefs",{"download.default_directory":"home/admin/Downloads"})
+driver=webdriver.Chrome("chromeoptions=chromeoptions")
+driver.get("http://demo.automationtesting.in/FileDownload.html")
+driver.maximize_window()
+#download text file
+driver.find_element_by_id("textbox").send_keys("testng download fie")
+driver.find_element_by_id("createTxt").click()
+driver.find_element_by_id("link-to-download").click()
+#download pdf file
+driver.find_element_by_id("pdfbox").send_keys("testng download fie")
+driver.find_element_by_id("createPdf").click()
+driver.find_element_by_id("pdf-link-to-download").click()
+
